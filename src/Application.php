d8bac360ffd675bb760a3dc828c23f353b03a5a8
Chidiebere Chukwudi <jollydosky@gmail.com>
2023-03-03 21:30:03 +0100

    setting up. Configuring plugin

--- src/Application.php
+++ src/Application.php
@@ -72,7 +72,7 @@ class Application extends BaseApplication
     /**
      * Setup the middleware queue your application will use.
      *
-     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
+     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup. 
      * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
      */
     public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue

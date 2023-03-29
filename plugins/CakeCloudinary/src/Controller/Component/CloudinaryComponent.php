9fa8c9a66ab07ade2d1378f9311ad539f0cf8bd5
jovialcore <jollydosky@gmail.com>
2023-03-29 21:40:40 +0100

    added more apis ---delete,

--- plugins/CakeCloudinary/src/Controller/Component/CloudinaryComponent.php
+++ plugins/CakeCloudinary/src/Controller/Component/CloudinaryComponent.php
@@ -12,6 +12,10 @@ use Cloudinary\Cloudinary;
 
 /**
  * Cloudinary component
+ * Reference:
+ * https://cloudinary.com/documentation/php_image_and_video_upload#server_side_upload
+ *
+ * another useful resource : https://cloudinary.com/documentation/image_upload_api_reference#upload
  */
 class CloudinaryComponent extends Component
 {
@@ -21,6 +25,7 @@ class CloudinaryComponent extends Component
      * @var array<string, mixed>
      */
     protected $_defaultConfig = [];
+
     private $cloudinary;
 
     protected $response;
@@ -46,18 +51,66 @@ class CloudinaryComponent extends Component
     }
 
 
-    // expose cloudinary upload functionality
+    // wrap the cloudinary upload functionality
 
     public function uploadApi()
     {
         return $this->cloudinary->uploadApi();
     }
 
+    // wrap the adminApi functionality
+
+    public function adminApi()
+    {
+        return $this->cloudinary->adminApi();
+    }
+
 
     // upload a file to ccloudinary account
 
-    public function upload()
+    public function upload($file, array $options = [])
     {
-        return $this->response = $this->uploadApi()->upload();
+        return $this->response = $this->uploadApi()->upload($file, $options);
     }
+
+    // upload any typeof file to cloudinary account
+
+    public function uploadFile($file, array $options = [])
+    {
+        $anyFileParams  = ['resource_type' =>  'auto'];
+
+        $options = array_merge($options, $anyFileParams);
+
+        return $this->response = $this->uploadApi()->upload($file, $options);
+    }
+
+
+    // delete an image  from cloudinray account
+
+    public function deleteImage($image, array $options = [])
+    {
+        return $this->response = $this->uploadApi()->destroy($image, $options);
+    }
+
+    // delete video from cloudinary
+
+    public function deleteVideo($video, array $options = [])
+    {
+        $videoparam  = ['resource_type' =>  'video'];
+
+        $options = array_merge($options, $videoparam);
+
+        return $this->response = $this->uploadApi()->destroy($video, $options);
+    }
+
+    /**
+     * TO dos:
+     * Create collages
+     * tags
+     * context,
+     * text api,
+     *
+     * and some functionalities like image video creation, and creation of collages that doesn't support collages yet
+     *
+     */
 }

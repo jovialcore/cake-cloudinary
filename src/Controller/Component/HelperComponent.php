0cd35b824b332536c6cae8c88d5d60336c0446af
Chidiebere Chukwudi <jollydosky@gmail.com>
2023-04-21 22:02:22 +0100

    Added a return keyword to get file format

--- src/Controller/Component/HelperComponent.php
+++ src/Controller/Component/HelperComponent.php
@@ -32,7 +32,7 @@ class HelperComponent extends Component
     {
         $i = floor(log($bytes, 1024));
 
-        round($bytes / pow(1024, $i), [0, 0, 2, 2, 3][$i]) . ['B', 'KB', 'MB', 'GB', 'TB'][$i];
+         return round($bytes / pow(1024, $i), [0, 0, 2, 2, 3][$i]) . ['B', 'KB', 'MB', 'GB', 'TB'][$i];
     }
 
 
